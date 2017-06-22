4df8ebbb2c2e46797f90229abf4ad1fb1200b7b3
Gustavo da Cruz <gecruz.dev@gmail.com>
2017-06-22 02:21:24 -0300

    refactor: change shoppingCart directive logic

--- app/directives/shoppingCartDirective/shoppingCartDirective.directive.js
+++ app/directives/shoppingCartDirective/shoppingCartDirective.directive.js
@@ -1,49 +1,18 @@
 import './shoppingCartDirective.view.html'
 
-class ShoppingCartController {
-  constructor ($scope, $state) {
-    'ngInject'
-
-    this._$scope = $scope
-    this._$state = $state
-    this._$scope.options = {
-      books: []
-    }
-  }
-
-  updateOptions () {
-    let scope = this._$scope
-    let keys = Object.keys(scope.options)
-    keys.forEach(key => {
-      scope.options[key] = this[key] || scope.options[key]
-    })
-  }
-
-  openCart () {
-    this._$state.go('app.shoppingCart', { books: this.books })
-  }
-
-  setTitle () {
-    this.title = 'Shopping Cart'
-  }
-}
-
-export class ShoppingCartDirective {
+export default class ShoppingCartDirective {
   constructor () {
-    'ngInject'
-
     this.templateUrl = 'directives/shoppingCartDirective/shoppingCartDirective.view.html'
     this.restrict = 'E'
-    this.controllerAs = 'ctrl'
-    this.controller = ShoppingCartController
-    this.bindToController = true
-    this.scope = {
-      books: '='
-    }
+    this.scope = { books: '=' }
   }
 
-  link ($scope, $element, $attr, $ctrl) {
-    $ctrl.updateOptions()
+  compile (tElement, done) {
+    tElement.css('position', 'relative')
+  }
+
+  link (scope, element, attributes) {
+    this.books = scope.books
   }
 
 }
