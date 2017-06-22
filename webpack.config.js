208b3218bf5764c83b65d3f1038ee1b4b0fb74e9
Gustavo da Cruz <gecruz.dev@gmail.com>
2017-06-22 02:18:20 -0300

    refactor: remove chunck for webpack hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-let webpack = require('webpack'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin'),
-  HtmlWebPackPlugin = require('html-webpack-plugin'),
-  path = require('path')
+let webpack = require('webpack')
+let ExtractTextPlugin = require('extract-text-webpack-plugin')
+let HtmlWebPackPlugin = require('html-webpack-plugin')
+let path = require('path')
 
 module.exports = {
   entry: {
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   output: {
-    filename: '[name].[hash].bundle.js',
+    filename: '[name].bundle.js',
     path: path.join(__dirname, 'dist')
   },
   plugins: [
@@ -31,43 +31,36 @@ module.exports = {
       test: /\.html$/,
       include: path.resolve(__dirname, 'app/'),
       loader: `ngtemplate-loader?relativeTo=${__dirname}/app/!html-loader`
-    },
-      {
-        test: /\index.html$/,
-        exclude: path.resolve(__dirname, 'node_modules/'),
-        use: 'html-loader?name=[name].[ext]'
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
-          publicPath: '/dist'
-        })
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-          publicPath: '/dist'
-        })
-      },
-      {
-        test: /\.(png|jpeg|jpg|gif)$/,
-        include: path.join(__dirname, 'app/images/'),
-        use: 'file-loader?name=images/[name].[ext]&context=app/images/'
-      },
-      {
-        test: /\.(woff|woff2|svg|eot|ttf)(\?.+)?$/i,
-        use: 'file-loader?name=[name].[ext]'
-      }
-    ]
+    }, {
+      test: /\index.html$/,
+      exclude: path.resolve(__dirname, 'node_modules/'),
+      use: 'html-loader?name=[name].[ext]'
+    }, {
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
+        publicPath: '/dist'
+      })
+    }, {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader',
+        publicPath: '/dist'
+      })
+    }, {
+      test: /\.(png|jpeg|jpg|gif)$/,
+      include: path.join(__dirname, 'app/images/'),
+      use: 'file-loader?name=images/[name].[ext]&context=app/images/'
+    }, {
+      test: /\.(woff|woff2|svg|eot|ttf)(\?.+)?$/i,
+      use: 'file-loader?name=[name].[ext]'
+    }]
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -75,11 +68,11 @@ module.exports = {
     inline: true,
     compress: true,
     stats: { colors: true },
-    clientLogLevel: "info"
+    clientLogLevel: 'error'
   },
   watchOptions: {
     aggregateTimeout: 300,
     ignored: path.resolve(__dirname, 'node_modules/')
   },
-  devtool: 'source-map',
+  devtool: 'source-map'
 }
